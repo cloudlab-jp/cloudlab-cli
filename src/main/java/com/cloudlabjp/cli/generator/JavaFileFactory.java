2643282a6960513ce55257263336b28df98f8327
juankxxD <juangamerospina@gmail.com>
2026-07-16 17:31:50 -0500

    refactor(cli): migrate Java generation to resource templates

--- src/main/java/com/cloudlabjp/cli/generator/JavaFileFactory.java
+++ src/main/java/com/cloudlabjp/cli/generator/JavaFileFactory.java
@@ -2,38 +2,48 @@ package com.cloudlabjp.cli.generator;
 
 import com.cloudlabjp.cli.generator.model.GeneratedFile;
 import com.cloudlabjp.cli.project.ProjectInfo;
-import com.cloudlabjp.cli.template.JavaTemplates;
+import com.cloudlabjp.cli.template.ResourceTemplateEngine;
 import com.cloudlabjp.cli.util.StringUtils;
 
 import java.util.List;
+import java.util.Map;
 
 public class JavaFileFactory {
+
+    private final ResourceTemplateEngine templateEngine =
+            new ResourceTemplateEngine();
 
     public List<GeneratedFile> create(ProjectInfo project,
                                       String module) {
 
         String className = StringUtils.capitalize(module);
 
+        Map<String, String> variables = Map.of(
+                "basePackage", project.basePackage(),
+                "module", module,
+                "className", className
+        );
+
         return List.of(
 
                 new GeneratedFile(
                         "application/service/" + className + "Service.java",
-                        JavaTemplates.service(project.basePackage(), module)
+                        templateEngine.render("service.java.tpl", variables)
                 ),
 
                 new GeneratedFile(
                         "domain/model/" + className + ".java",
-                        JavaTemplates.entity(project.basePackage(), module)
+                        templateEngine.render("entity.java.tpl", variables)
                 ),
 
                 new GeneratedFile(
                         "domain/repository/" + className + "Repository.java",
-                        JavaTemplates.repository(project.basePackage(), module)
+                        templateEngine.render("repository.java.tpl", variables)
                 ),
 
                 new GeneratedFile(
                         "infrastructure/controller/" + className + "Controller.java",
-                        JavaTemplates.controller(project.basePackage(), module)
+                        templateEngine.render("controller.java.tpl", variables)
                 )
 
         );
